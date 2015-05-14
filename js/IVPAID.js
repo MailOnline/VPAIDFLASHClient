deea2fa918a1aa955ba1ad687372af700cb509e9
Luis Silva <luis.dasilva@mailonline.co.uk>
2015-05-14 17:16:24 +0100

    [Luis] remove bower_components, add gitignore, add a way to handle callbacks, and events

--- js/IVPAID.js
+++ js/IVPAID.js
@@ -1,10 +1,25 @@
 //simple representation of the API
 export class IVPAID {
 
-    //async methods
-    handshakeVersion() {}
-    initAd () {}
-    resizeAd() {}
+    //custom implementation, sync methods
+    getWidth() {}
+    setWidth(w) {}
+
+    getHeight() {}
+    setHeight(h) {}
+
+    getSize() {}
+    setSize(width, height) {}
+
+
+    //all methods below
+    //are async methods
+    handshakeVersion(callback, playerVPAIDVersion = '2.0') {}
+
+    //width and height is not in the beginning because we will use the default width/height used in the constructor
+    initAd ( viewMode : String, desiredBitrate : Number, width = 0, height = 0, creativeData = '', environmentVars = '') {}
+    resizeAd(width, height, viewMode) {}
+
     startAd() {}
     stopAd() {}
     pauseAd() {}
@@ -21,8 +36,33 @@ export class IVPAID {
     adSkippableState() {}
     adRemainingTime() {}
     adDuration() {}
-    adVolume() {}
+    setAdVolume(soundVolume) {}
+    getAdVolume() {}
     adCompanions() {}
     adIcons() {}
 }
+
+/*
+Events that can be subscribed
+AdLoaded
+AdStarted
+AdStopped
+AdSkipped
+AdSkippableStateChange
+AdSizeChange
+AdLinearChange
+AdDurationChange
+AdExpandedChange
+AdRemainingTimeChange [Deprecated in 2.0] but will be still fired for backwards compatibility
+AdVolumeChange
+AdImpression
+AdVideoStart, AdVideoFirstQuartile, AdVideoMidpoint, AdVideoThirdQuartile,
+AdVideoComplete
+AdClickThru
+AdInteraction
+AdUserAcceptInvitation, AdUserMinimize, AdUserClose
+AdPaused, AdPlaying
+AdLog
+AdError
+*/
 
--- js/flashVPAID.js
+++ js/flashVPAID.js
@@ -1,6 +1,10 @@
+//if this code already run once don't do anything
+if (window.FlashVPAID) return;
+
 let IVPAID = require('./IVPAID').IVPAID;
 let noop = require('./utils').noop;
-let unique = require('./utils').unique('vpaid');
+let unique = require('./utils').unique;
+let uniqueVPAID = unique('vpaid');
 let instances = {};
 const VPAID_FLASH_HANDLER = 'vpaid_video_flash_handler';
 
@@ -12,13 +16,16 @@ function createElementWithID(parent, id) {
     return nEl;
 }
 
-//TODO: check if the swf will be in the same domain, but I think will not be...
 class FlashVPAID extends IVPAID {
     constructor (vpaidWrapper, callback, swfConfig = {data: 'VPAIDFlash.swf', width: 800, height: 400}, version = '9', params = { wmode: 'transparent', salign: 'tl', allowScriptAccess: 'always'}, debug = false) {
         super();
+        this._handlers = {};
+        this._callbacks = {};
+
         this.vpaidWrapper = vpaidWrapper;
-        this.flashID = unique();
+        this.flashID = uniqueVPAID();
         this.load =  callback || noop;
+        this._uniqueMethodIdentifier = unique(flashID);
         createElementWithID(vpaidWrapper, this.flashID);
 
         //because flash externalInterface will call
@@ -34,14 +41,120 @@ class FlashVPAID extends IVPAID {
         //if this.el is undefined means swfobject failed to create the swfobject
         if (!this.el) return this;
     }
+
+    _safeFlashMethod(methodName, args = [], callbacks = undefined) {
+        var callbackID = '';
+        // if no callback, some methods the return is void so they don't need callback
+        if (callback) {
+            var callbackID = this.uniqueMethodIdentifier();
+            this._callbacks[callbackID] = callback;
+        }
+
+        try {
+            this.el[methodName].call(this, [this.flashID, methodName, callbackID].concat(args));
+
+        } catch (e) {
+            if (callback) {
+                delete this.callback[callbackID];
+                callback(e);
+            } else {
+
+                //if there isn't any callback to return error use error event handler
+                _fireEvent('error', [e]);
+            }
+        }
+    }
+
+    _flashMethodAnswer(methodName, callbackID, args) {
+
+        //method's that return void will not have callbacks
+        if (callbackID === '') return;
+
+        if (!this._callbacks[callbackID]) {
+            //TODO: something is wrong, this should never happens if it happens fire an error
+            return;
+        }
+
+        //TODO: check with carlos if we need to use apply instead
+        this._callbacks[callbackID](args);
+        delete this._callbacks[callbackID];
+    }
+
+    _fireEvent(eventName, args) {
+        //TODO: check if forEach and isArray is added to the browser with babeljs
+        if (Array.isArray(this._handlers[eventName])) {
+            this._handlers[eventName].forEach(function (callback) {
+                setTimeout(function () {
+                    callback(args);
+                }, 0);
+            });
+        }
+    }
+
+    on(eventName, callback) {
+        if (!this._handlers[eventName]) {
+            this._handlers[eventName] = [];
+        }
+        this._handlers[eventName].push(callback);
+    }
+
+    //async methods
+    handshakeVersion(callback, playerVPAIDVersion = '2.0') {
+        _safeFlashMethod('handshakeVersion', [playerVPAIDVersion], callback);
+    }
+
+    initAd (viewMode, desiredBitrate, width = 0, height = 0, creativeData = '', environmentVars = '') {
+        this.size(width, height);
+        _safeFlashMethod('initAd', [this.getWidth(), this.getHeight(), viewMode, desiredBitrate, creativeData, environmentVars]);
+    }
+
+    resizeAd(width, height, viewMode) {
+        this.size(width, height);
+        _safeFlashMethod('resizeAd', [this.getWidth(), this.getHeight(), viewMode]);
+    }
+
+    startAd() {
+        _safeFlashMethod('startAd');
+    }
+    stopAd() {
+        _safeFlashMethod('stopAd');
+    }
+    pauseAd() {
+        _safeFlashMethod('pauseAd');
+    }
+    resumeAd() {
+        _safeFlashMethod('resumeAd');
+    }
+    expandAd() {
+        _safeFlashMethod('expandAd');
+    }
+    collapseAd() {
+        _safeFlashMethod('collapseAd');
+    }
+    skipAd() {
+        _safeFlashMethod('skipAd');
+    }
+
+    //properties that will be treat as async methods
+    adLinear() {}
+    adWidth() {}
+    adHeight() {}
+    adExpanded() {}
+    adSkippableState() {}
+    adRemainingTime() {}
+    adDuration() {}
+    adVolume() {}
+    adCompanions() {}
+    adIcons() {}
+
     _flash_handShake (message) {
         console.log('handShake:', message);
         if (message == 'prepared') {
             this.load();
         }
     }
-}
 
+}
 
 window[VPAID_FLASH_HANDLER] = function (flashID, event, message) {
     console.log('flashID', flashID, 'event', event, 'message', message);
