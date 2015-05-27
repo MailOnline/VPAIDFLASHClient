a937323520a3fcddd7246555ab62c6a32ec8a3de
Luis Silva <luis.dasilva@mailonline.co.uk>
2015-05-27 12:16:20 +0100

    [Luis] add more tests to adUnit

--- test/VPAIDAdUnit.spec.js
+++ test/VPAIDAdUnit.spec.js
@@ -5,10 +5,11 @@ let noop = require('./testHelper.js').noop;
 describe('VPAIDAdUnit.js api', function() {
     let adUnit;
     let flash;
+    let el;
     const EL_ID = 'hello';
 
     beforeEach(function() {
-        let el = document.createElement('div');
+        el = document.createElement('div');
         flash = new JSFlashBridge(el, '', EL_ID, 10, 10, noop);
         adUnit = new VPAIDAdUnit(flash);
     });
@@ -23,6 +24,28 @@ describe('VPAIDAdUnit.js api', function() {
             adUnit[methodName](randomEvent, noop);
             assert(flashMethod.calledWith(randomEvent, noop));
         });
+    });
+
+    it('must implement handshakeVersion', function () {
+        let flashMethod = sinon.stub(flash, 'callFlashMethod');
+        adUnit.handshakeVersion('1.1', noop);
+        assert(flashMethod.calledWith('handshakeVersion', ['1.1'], noop));
+    });
+
+    it('must implement initAd', function () {
+        let flashMethod = sinon.stub(flash, 'callFlashMethod');
+        adUnit.initAd('thumbnail', -1, 100, 200, '', '', noop);
+        assert(flashMethod.calledWith('initAd', [100, 200, 'thumbnail', -1, '', ''], noop));
+        assert.equal(el.getAttribute('width'), '100');
+        assert.equal(el.getAttribute('height'), '200');
+    });
+
+    it('must implement resizeAd', function () {
+        let flashMethod = sinon.stub(flash, 'callFlashMethod');
+        adUnit.resizeAd(300, 200, 'normal', noop);
+        assert(flashMethod.calledWith('resizeAd', [300, 200, 'normal'], noop));
+        assert.equal(el.getAttribute('width'), '300');
+        assert.equal(el.getAttribute('height'), '200');
     });
 
     [
